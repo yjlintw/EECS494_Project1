c22f4c08682b9c4ceea84f9cb5e6c012b81dae21
Yu-Jen Lin <peacedove5210@gmail.com>
2016-01-16 16:13:42 -0500

    build the map to first check point

---
+++ Assets/__Scripts/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+    public GameObject spawnPrefab;
+    public bool useGravity = true;
+    private GameObject spawnedGameObject;
+	// Use this for initialization
+	void Start () {
+        spawnedGameObject = null;
+	    Spawn();
+	}
+    
+    public void Spawn() {
+        if (spawnedGameObject == null) {
+            spawnedGameObject = (GameObject)Instantiate(spawnPrefab, gameObject.transform.position, Quaternion.identity);
+            spawnedGameObject.GetComponent<Rigidbody>().useGravity = useGravity;
+        }
+    }
+}
