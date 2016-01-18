8cf3b732cb88d22ffd5daed3b6dc2838f5f57923
Yu-Jen Lin <peacedove5210@gmail.com>
2016-01-18 14:15:34 -0500

    turtle flipped when launch

--- Assets/__Scripts/TurtleEnemy.cs
+++ Assets/__Scripts/TurtleEnemy.cs
@@ -57,6 +57,8 @@ public class TurtleEnemy : Enemy {
         if (col.gameObject.tag == Tags.CRASH) {
             if (Crash.S.spinning) {
                 LaunchEnemy();
+                flipped = true;
+                Flip(flipped);
                 return;
             }
             
